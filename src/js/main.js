835da0ad130c212a54d987fa78584a5c038f9e3b
SHAKIB <shakibzero95@gmail.com>
2025-08-22 18:20:23 +0330

    configure Vite and Alpine.js for project

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,36 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 20,
-    loop: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
+import Alpine from "alpinejs";
+import Swiper from "swiper";
+import "swiper/css";
+import '../input.css'
+
+window.Alpine = Alpine;
+Alpine.start();
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".custom-next",
+    prevEl: ".custom-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    768: {
+      slidesPerView: 2,
     },
-    navigation: {
-      nextEl: ".custom-next",
-      prevEl: ".custom-prev",
+    1024: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 2,
-      },
-    }
-  });
+  },
+});
